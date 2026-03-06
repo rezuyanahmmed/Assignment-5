358407527b4c3b730c1765fb68a37b5a626011fe
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-03-07 00:59:49 +0600

    commit

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('signin').addEventListener('click', function () {
+  let userName = document.getElementById('username').value;
+  let userPassword = document.getElementById('userPass').value;
+
+  // let user = username.value;
+  // let password = userPass.value;
+
+  if (userName === 'admin' && userPassword === 'admin123') {
+    window.location.assign('home.html');
+  } else {
+    alert('Inavalid Username or Password');
+  }
+
+
+});
