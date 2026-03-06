71e3b546e08ccf2c4751a04078a1f2a3cd5c9d5a
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-03-07 01:13:58 +0600

    fix login script

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 document.getElementById('signin').addEventListener('click', function () {
-  let userName = document.getElementById('username').value;
-  let userPassword = document.getElementById('userPass').value;
 
-  // let user = username.value;
-  // let password = userPass.value;
+  let userName = document.getElementById('username').value.trim();
+  let userPassword = document.getElementById('userPass').value.trim();
 
   if (userName === 'admin' && userPassword === 'admin123') {
-    window.location.assign('./home.html');
+    window.location.href = "./home.html";
   } else {
-    alert('Inavalid Username or Password');
+    alert('Invalid Username or Password');
   }
-
 
 });
